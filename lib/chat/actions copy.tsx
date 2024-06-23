1462fc4d3ddda612c24779723b6b9ef5d215af85
hanmillee <94hanmil@gmail.com>
2024-06-23 21:51:33 +0900

    qdrant 및 context 추가

---
+++ lib/chat/actions copy.tsx
@@ -0,0 +1,223 @@
+import 'server-only'
+
+// 필요한 모듈들 임포트
+import {
+  createAI,
+  createStreamableUI,
+  getMutableAIState,
+  getAIState,
+  streamUI,
+  createStreamableValue
+} from 'ai/rsc'
+import { openai } from '@ai-sdk/openai'
+
+import { spinner, BotMessage, SystemMessage } from '@/components/stocks'
+
+import { z } from 'zod'
+import {
+  formatNumber,
+  runAsyncFnWithoutBlocking,
+  sleep,
+  nanoid
+} from '@/lib/utils'
+import { saveChat } from '@/app/actions'
+import { SpinnerMessage, UserMessage } from '@/components/stocks/message'
+import { Chat, Message } from '@/lib/types'
+import { auth } from '@/auth'
+
+// 주식 구매 확인 함수
+async function confirmPurchase(symbol: string, price: number, amount: number) {
+  'use server'
+
+  const aiState = getMutableAIState<typeof AI>()
+
+  const purchasing = createStreamableUI(
+    <div className="inline-flex items-start gap-1 md:items-center">
+      {spinner}
+      <p className="mb-2">
+        Purchasing {amount} ${symbol}...
+      </p>
+    </div>
+  )
+
+  const systemMessage = createStreamableUI(null)
+
+  runAsyncFnWithoutBlocking(async () => {
+    await sleep(1000)
+
+    purchasing.update(
+      <div className="inline-flex items-start gap-1 md:items-center">
+        {spinner}
+        <p className="mb-2">
+          Purchasing {amount} ${symbol}... working on it...
+        </p>
+      </div>
+    )
+
+    await sleep(1000)
+
+    purchasing.done(
+      <div>
+        <p className="mb-2">
+          You have successfully purchased {amount} ${symbol}. Total cost:{' '}
+          {formatNumber(amount * price)}
+        </p>
+      </div>
+    )
+
+    systemMessage.done(
+      <SystemMessage>
+        You have purchased {amount} shares of {symbol} at ${price}. Total cost ={' '}
+        {formatNumber(amount * price)}.
+      </SystemMessage>
+    )
+
+    aiState.done({
+      ...aiState.get(),
+      messages: [
+        ...aiState.get().messages,
+        {
+          id: nanoid(),
+          role: 'system',
+          content: `[User has purchased ${amount} shares of ${symbol} at ${price}. Total cost = ${
+            amount * price
+          }]`
+        }
+      ]
+    })
+  })
+
+  return {
+    purchasingUI: purchasing.value,
+    newMessage: {
+      id: nanoid(),
+      display: systemMessage.value
+    }
+  }
+}
+
+// 사용자 메시지 제출 함수
+async function submitUserMessage(content: string) {
+  'use server'
+
+  const aiState = getMutableAIState<typeof AI>()
+
+  aiState.update({
+    ...aiState.get(),
+    messages: [
+      ...aiState.get().messages,
+      {
+        id: nanoid(),
+        role: 'user',
+        content
+      }
+    ]
+  })
+
+  let textStream: undefined | ReturnType<typeof createStreamableValue<string>>
+  let textNode: undefined | React.ReactNode
+
+  const result = await streamUI({
+    model: openai('gpt-3.5-turbo'),
+    initial: <SpinnerMessage />,
+    system: `\
+    무엇이든 친절하게 답변해줘`,
+    messages: [
+      ...aiState.get().messages.map((message: any) => ({
+        role: message.role,
+        content: message.content,
+        name: message.name
+      }))
+    ],
+    text: ({ content, done, delta }) => {
+      if (!textStream) {
+        textStream = createStreamableValue('')
+        textNode = <BotMessage content={textStream.value} />
+      }
+
+      if (done) {
+        textStream.done()
+        aiState.done({
+          ...aiState.get(),
+          messages: [
+            ...aiState.get().messages,
+            {
+              id: nanoid(),
+              role: 'assistant',
+              content
+            }
+          ]
+        })
+      } else {
+        textStream.update(delta)
+      }
+
+      return textNode
+    }
+  })
+
+  return {
+    id: nanoid(),
+    display: result.value
+  }
+}
+
+// AI 상태 타입 정의
+export type AIState = {
+  chatId: string
+  messages: Message[]
+}
+
+// AI 생성 함수
+export const AI = createAI<AIState>({
+  actions: {
+    submitUserMessage,
+    confirmPurchase
+  },
+  initialUIState: [],
+  initialAIState: { chatId: nanoid(), messages: [] },
+  onGetUIState: async () => {
+    'use server'
+
+    const session = await auth()
+
+    if (session && session.user) {
+      const aiState = getAIState()
+
+      if (aiState) {
+        return aiState.messages.map(message => message.content).join('\n')
+      }
+    } else {
+      return
+    }
+  },
+  onSetAIState: async ({ state }) => {
+    'use server'
+
+    const session = await auth()
+
+    if (session && session.user) {
+      const { chatId, messages } = state
+
+      const createdAt = new Date()
+      const userId = session.user.id as string
+      const path = `/chat/${chatId}`
+
+      const firstMessageContent = messages[0].content as string
+      const title = firstMessageContent.substring(0, 100)
+
+      const chat: Chat = {
+        id: chatId,
+        title,
+        userId,
+        createdAt,
+        messages,
+        path
+      }
+
+      await saveChat(chat)
+    } else {
+      return
+    }
+  }
+})
